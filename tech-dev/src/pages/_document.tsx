145822167729c2dea172c28b22fe790345d450e0
João Walter Amadeu <joao.amadeu@unifio.edu.br>
2023-01-23 14:52:12 -0300

    Added icon, manifest

--- tech-dev/src/pages/_document.tsx
+++ tech-dev/src/pages/_document.tsx
@@ -1,12 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { ColorModeScript, useColorModeValue } from '@chakra-ui/react'
 
-
 export default function Document() {
   
   return (
     <Html lang="pt">
-      <Head><link rel="icon" href="src/public/images/brazil-flag-round-icon-256.png" /></Head>
+      <Head></Head>
       <body>
       <ColorModeScript initialColorMode={'light'} />
         <Main />
